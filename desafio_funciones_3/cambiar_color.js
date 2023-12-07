74f5a05a7b8dc1d9714fd151f5ad1983c435ff01
Julio Mendoza <juliofacundo2019@gmail.com>
2023-12-07 02:58:12 -0300

    actualizacion desafio_funciones_3

--- desafio_funciones_3/cambiar_color.js
+++ desafio_funciones_3/cambiar_color.js
@@ -1,22 +1,40 @@
 /**  guarde dentro de una variable global
 un color dependiendo de la letra del teclado 
 presionada. */
-var color = document.getElementById('#key')
+var color;
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'a') {
-    /* Cambiar a pink */
-    key.addEventListener.style.backgroundColor='pink'
+        /* Cambiar a pink */
+        color= document.getElementById('key').style.backgroundColor ='pink'
+    
+        } else if (event.key === 's') {
+        /* Cambiar a orange */
+        color= document.getElementById('key').style.backgroundColor ='orange'
+    
+        } else if(event.key ==='d'){
+        /* Cambiar a blueLight */
+        color= document.getElementById('key').style.backgroundColor ='aqua'
+        }  
+        
+    })
 
-    } else if (event.key === 's') {
-    /* Cambiar a orange */
-    document.key.style.backgroundColor='orange';
+const container = document.getElementById("container");
+const newDiv = document.createElement("div")
 
-    } else if(event.key ==='d'){
-    /* Cambiar a blueLight */
-    document.key.style.backgroundColor='blueLight';
-    }        
-})
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'q') {
+        /* Cambiar a purple */
+        newDiv.style="background-color: purple; border: solid 5px black; height: 200px; width: 200px"
+        
+    } else if (event.key === 'w') {
+        /* Cambiar a grey */
+        newDiv.style="background-color: grey; ; border: solid 5px black;height: 200px; width: 200px"
 
+    } else if(event.key ==='e'){
+        /* Cambiar a brown */
+        newDiv.style="background-color: brown;; border: solid 5px black; height: 200px; width: 200px"
+    }  
+    })
 
-var key = document.getElementById('#key') = color;
+container.appendChild(newDiv)
